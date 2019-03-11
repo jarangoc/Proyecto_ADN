a45a6e7f54849acc56141147c8e6a2b85b86bc10
jarangoc <juan.arango@ceiba.com.co>
2019-03-11 09:13:58 -0500

    Primer commit

---
+++ src/main/java/com/co/ceiba/entrenamiento/ProyectoAdnApplication.java
@@ -0,0 +1,13 @@
+package com.co.ceiba.entrenamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/ceiba/entrenamiento/ProyectoAdnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.co.ceiba.entrenamiento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoAdnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
