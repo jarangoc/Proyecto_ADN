d3edaf27c66a1668eae06269de76ca0492d4b08c
jarangoc <juan.arango@ceiba.com.co>
2019-03-12 10:24:17 -0500

    Inicio desarrollo parqueadero, creación de clases principales

---
+++ src/main/java/com/co/ceiba/entrenamiento/entities/RegistroParqueadero.java
@@ -0,0 +1,103 @@
+package com.co.ceiba.entrenamiento.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class RegistroParqueadero {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@OneToOne
+	private Vehiculo vehiculo;
+	
+	@Temporal (TemporalType.TIME)
+	private Date fechaIngreso;
+	
+	@Temporal (TemporalType.TIME)
+	private Date fechaSalida;
+	
+	@NotEmpty
+	@Column
+	private String estadoRegistro;
+	
+	@Column
+	private Double valorCobrado;
+	
+	
+	public RegistroParqueadero() {
+		
+	}
+
+	public RegistroParqueadero(Long id, Vehiculo vehiculo, Date fechaIngreso, Date fechaSalida,
+			String estadoRegistro, Double valorCobrado) {
+		this.id = id;
+		this.vehiculo = vehiculo;
+		this.fechaIngreso = fechaIngreso;
+		this.fechaSalida = fechaSalida;
+		this.estadoRegistro = estadoRegistro;
+		this.valorCobrado = valorCobrado;
+	}
+	
+	//GETTERS AND SETTERS
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public String getEstadoRegistro() {
+		return estadoRegistro;
+	}
+
+	public void setEstadoRegistro(String estadoRegistro) {
+		this.estadoRegistro = estadoRegistro;
+	}
+
+	public Double getValorCobrado() {
+		return valorCobrado;
+	}
+
+	public void setValorCobrado(Double valorCobrado) {
+		this.valorCobrado = valorCobrado;
+	}
+	
+}
